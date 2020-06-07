f42110ad88bd0c7e546bab655dea19412d0aabc9
jathinbadam <jathinbadam99@gmail.com>
2020-06-07 18:56:40 +0530

    GET/POST not working

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cookieparser = require('cookie-parser');
+const mongoose  = require('mongoose');
+const Database = require("./config/keys.js")
+const userRouter = express.Router();
+
+app.use(cookieparser);
+app.use(express.json);
+
+
+//DB Config
+const db = Database.MongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connectec..."))
+  .catch((err) => console.log(err));
+
+  //app.use('/user', require('./routes/User'));
+
+  userRouter.get('/user',(req,res)=>  
+  {
+   console.log("wokring");
+   return res.status(200).json({message : {msgBody : 'You are an admin', msgError : false}});
+  }); 
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => 
+  {
+  console.log(`Server started on Port ${PORT}`)
+  });
+  
